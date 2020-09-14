1ecf56ecdf5ee0a612599013f27393ee42d81300
Jayson Mulwa <jayson.mulwa@gmail.com>
2020-09-14 21:01:05 +0300

    Create maximum_subarray.js

---
+++ arrays/maximum_subarray.js
@@ -0,0 +1,10 @@
+//kadane's algorithm
+var maxSubArray = function(arr){
+  let a1 = 0
+  let a2 = arr[0]
+  arr.forEach((i,a) => {
+    a1 = Math.max(i, a1 + i)
+    a2 = Math.max(a2, a1)
+  })
+  return a2
+}
