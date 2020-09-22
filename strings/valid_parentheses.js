925bd207fa370dac5464a797fecd8bdc99c97694
Jayson Mulwa <jayson.mulwa@gmail.com>
2020-09-22 17:24:16 +0300

    Create valid_parentheses.js

---
+++ strings/valid_parentheses.js
@@ -0,0 +1,62 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function(s) {
+  let stack = new Stack();
+
+  for (let character of s) {
+    if (character === '{' || character === '[' || character === '(') {
+      stack.push(character);
+    } else if (!stack.isEmpty() && character === '}' && stack.peek() === '{') {
+      stack.pop();
+    } else if (!stack.isEmpty() && character === ')' && stack.peek() === '(') {
+      stack.pop();
+    } else if (!stack.isEmpty() && character === ']' && stack.peek() === '[') {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// manually created a stack since javascript doesn't have one built in
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  push(val) {
+    this.storage[this.count] = val;
+    this.count++;
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count--;
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  peek() {
+    let result = this.storage[this.count - 1];
+    return result;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+}
